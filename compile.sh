52ad4e2af1c1b7a6855893e947514771a060042f
Yahiaoui Rachid <rac021@hotmail.com>
2018-07-10 14:49:53 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@
 
   rm bin_test/jax-y-thorntail.jar
 
-  cd $CURRENT_LOCATION/source/dep/01_G-Jax-Swar-Api && -Dmaven.test.skip=true clean install
+  cd $CURRENT_LOCATION/source/dep/01_G-Jax-Swar-Api && mvn -Dmaven.test.skip=true clean install
 
   cd $CURRENT_LOCATION/source/dep/02_G-Jax-Security-Provider && mvn clean install
 
