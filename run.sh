e5ed291644b68113db3310ffb4c5a2d461f7fbc4
Yahiaoui Rachid <rac021@hotmail.com>
2018-07-10 14:42:38 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@
 
  jobs &>/dev/null
  
-   java -jar jax-y-thorntail.jar
+ java -jar jax-y-thorntail.jar
 
-  # java -Xdebug -Xrunjdwp:transport=dt_socket,address=11555,server=y,suspend=y -jar jax-y-thorntail.jar
+ # java -Xdebug -Xrunjdwp:transport=dt_socket,address=11555,server=y,suspend=y -jar jax-y-thorntail.jar
 
 
